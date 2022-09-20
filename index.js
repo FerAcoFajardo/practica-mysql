652eb62926c84e6a6ff74637e2095a05925f5749
FerAcoFajardo <fernando.acosta216555@potros.itson.edu.mx>
2022-09-19 21:12:18 -0700

    Finished crud

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
 import {Student} from "./src/domain/student.js";
 import {StudentRepository} from "./src/data/repositories/student.repository.js";
 
-const student = Student.build({name: "Fernando"});
+// const student = Student.build({name: "Fernando"});
 
 const studentRepository = new StudentRepository()
 
-studentRepository.create(student);
-// studentRepository.readAll().then((result) => {
-//     console.table(result);
-//     }).catch((err) => {
-//         console.log(err);
-//     });
+// studentRepository.create(student);
+// console.log(await studentRepository.readAll());
 
-// studentRepository.read(1).then((result) => {
-//     console.table(result);
-//     }).catch((err) => {
-//         console.log(err);
-//     });
+// let result = await studentRepository.read(3)
+// console.log(result)
 
-// const student2 = new Student("1", "Pedro Perez");
+// const student2 = Student.build({id: 1, name: "Fernando"});
 
 // studentRepository.update(student2);
 
-// studentRepository.delete(3);
+studentRepository.delete(3);
 
 // async function init(){
 //     const result = await studentRepository.readAll();
--- src/data/repositories/student.repository.js
+++ src/data/repositories/student.repository.js
@@ -3,14 +3,6 @@ import { Student } from "../../domain/student.js";
 
 class StudentRepository{
     
-    // constructor(){
-    //     this._connection = sequelize;
-    // }
-
-    // get connection(){
-    //     return this._connection;
-    // }
-
     async create(student){
         try{
             const result = await Student.create({name:student.name});
@@ -20,115 +12,59 @@ class StudentRepository{
             console.log(error)
         }
 
-        // console.log(result.toJSON());
-        // this.connection.connect((err) => {
-        //     if(err){
-        //         console.log("Error connecting to database");
-        //         return;
-        //     }
-        //     const sqlCode = `INSERT INTO students(name) VALUES('${student.name}')`;
-        //     this.connection.query(sqlCode, (error, result) => {
-        //         if(error){
-        //             console.log("Insert error");
-        //             return;
-        //         }
-        //         console.log("Student successfully created");
-        //     });
-        //     this.connection.end();
-        // });
     }
 
-    update(student){
-        this.connection.connect((err) => {
-            if(err){
-                console.log("Error connecting to database");
-                return;
-            }
-            const sqlCode = `UPDATE students SET name = '${student.name}' WHERE id = ${student.id}`;
-            this.connection.query(sqlCode, (error, result) => {
-                if(error){
-                    console.log("Update error");
-                    return;
+    async update(student){
+        console.log(student.id)
+
+        if (student.id === undefined){
+            throw new Error("Student id is required");
+        }
+        try{
+            const result = await Student.update({name:student.name}, {
+                where: {
+                    id: student.id
                 }
-                console.log("Student successfully updated");
             });
-            this.connection.end();
-        });
+            return result
+        }catch(error){
+            console.log(error)
+        }
     }
 
-    read(id){
-        return new Promise((resolve, reject) => {
-            this.connection.connect((err) => {
-                if(err){
-                    console.log(err);
-                    reject(err);
-                    return;
+    async read(id){
+        try{
+            const stundent = await Student.findAll({
+                where: {
+                    id: id
                 }
-                const sqlCode = `SELECT id, name from students where id = ${id}`;
-                this.connection.query(sqlCode, (error, result) => {
-                    if(error){
-                        console.log(err);
-                        reject(err);
-                        return;
-                    }
-                    const students = result.map((row) => {
-                        // return new Student(row.id, row.name);
-                        const {id, name} = row;
-                        return new Student(id, name);
-                    });
-                    resolve(students);
-                
-                });
-                this.connection.end();
             });
-        });
+            return stundent;
+        }catch(error){
+            console.log(error)
+        }
     }
 
 
-    readAll(){
-        return new Promise((resolve, reject) => {
-            this.connection.connect((err) => {
-                if(err){
-                    console.log(err);
-                    reject(err);
-                    return;
-                }
-                const sqlCode = `SELECT id, name from students `;
-                this.connection.query(sqlCode, (error, result) => {
-                    if(error){
-                        console.log(err);
-                        reject(err);
-                        return;
-                    }
-                    const students = result.map((row) => {
-                        // return new Student(row.id, row.name);
-                        const {id, name} = row;
-                        return new Student(id, name);
-                    });
-                    resolve(students);
-                
-                });
-                this.connection.end();
-            });
-        });
+    async readAll(){
+        try{
+            const students = await Student.findAll();
+            return students;
+        }catch(error){
+            console.log(error)
+        }
     }
 
-    delete(id){
-        this.connection.connect((err) => {
-            if(err){
-                console.log("Error connecting to database");
-                return;
-            }
-            const sqlCode = `DELETE FROM students WHERE id = ${id}`;
-            this.connection.query(sqlCode, (error, result) => {
-                if(error){
-                    console.log("Delete error");
-                    return;
+    async delete(id){
+        try{
+            await Student.destroy({
+                where: {
+                    id: id
                 }
-                console.log("Student successfully deleted");
             });
-            this.connection.end();
-        });
+        }catch(error){
+            console.log(error)
+        }
     }
 }
 
